a9973dbeed45c23ec2cf4cbe18e2ab9c3c311c37
NastassiaHaladushka <nhaladushka@gmail.com>
2023-01-30 15:18:34 +0100

    ref: change homework 5

--- lesson_05/homework-05.js
+++ lesson_05/homework-05.js
@@ -1,18 +1,22 @@
 let num = 13;
 let str = 'thirteen';
 let bool = false;
+
 //Выполнить сложение различных типов(string+boolean, string+number, number+boolean):
 let summ1 = str + bool;
 let summ2 = str + num;
 let summ3 = num + bool;
+
 //Выполнить умножение различных типов(string * boolean, string * number, number * boolean):
 let mult1 = str * bool;
 let mult2 = str * num;
 let mult3 = num * bool;
+
 //Выполнить деление различных типов(string/boolean, string/number, number/Boolean):
 let div1 = str / bool;
 let div2= str / num;
 let div3= num / bool;
+
 //Выполнить явное преобразование(number, string, boolean):
 num = String(num);
 num = Boolean(num);
