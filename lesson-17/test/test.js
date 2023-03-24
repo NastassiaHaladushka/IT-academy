da09e6d236656c19b4203a38948459ad12e3d927
NastassiaHaladushka <nhaladushka@gmail.com>
2023-03-24 16:30:04 +0100

    fix hw17

--- lesson-17/test/test.js
+++ lesson-17/test/test.js
@@ -18,6 +18,39 @@ describe('WebDriver test', function () {
         const title = await driver.findElement(By.xpath('//*[@id="h.p_ID_13"]'));
         expect(await title.getText()).to.contain('ChromeDriver');
     });
-
     
+    it('There should be a Chrome Extensions text', async () => {
+        await driver.get('https://chromedriver.chromium.org/home');
+        const menuChromeExtensions = await driver.findElements(By.xpath('//*[@id="WDxLfe"]/ul/li[3]'));
+        await menuChromeExtensions[0].click();
+        let titleColor = driver.findElement(By.xpath('//h1'));
+        await driver.executeScript("arguments[0].style.background='red'", titleColor)
+        const titleText = await driver.findElement(By.xpath('//h1'));
+        expect(await titleText.getText()).to.equal('Chrome Extensions');
+
+    });
+ 
+    it('There should be that the first link contains the word driver', async () => {
+        await driver.get('https://chromedriver.chromium.org/home');
+        const menuSearch = await driver.findElements(By.xpath('//*[@aria-label="Открыть поле поиска"]'));
+        await menuSearch[0].click();
+        const search = await driver.findElement(By.xpath('//*[@aria-label="Поиск по сайту"]'));
+        await search.sendKeys("driver")
+        await driver.actions().sendKeys(Key.ENTER).perform();
+        const linksDriver = await driver.findElements(By.xpath('//*[@jscontroller="U720xd"]/div[1]'));
+        expect(await linksDriver[0].getText()).to.contain('driver');
+    
+    });
+
+    it('There should be that url contains /mobile-emulation', async () => {
+        await driver.get('https://chromedriver.chromium.org/home');
+        const menuDop = await driver.findElements(By.xpath('//li[@class="VsJjtf oXBWEc"]'));
+        await menuDop[0].click();
+        const menuMobile = await driver.findElements(By.xpath('//ul[@class="VcS63b"]//li[10]'));
+        await menuMobile.click();
+        const urlMobile = await driver.getCurrentUrl();
+        expect(urlMobile).to.contain("/mobile-emulation");
+        
+    });
+
 });
