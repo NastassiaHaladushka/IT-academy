2426de6bd8783272c28a97e0bc8f996c32f4e308
NastassiaHaladushka <nhaladushka@gmail.com>
2023-04-08 08:12:52 +0200

    fix project

--- lesson-19/test/specs/test.js
+++ lesson-19/test/specs/test.js
@@ -1,9 +1,9 @@
-const { expect } = require('chai');
+const mainPage = require('../pageobjects/mainPage');
 
 describe('Checking the web site webdriver.io', () => {
 
   it('Should be found on the page Boilerplate and check the text amiya-pattnaik/webdriverIO-with-mochaBDD', async () => {
-    await browser.url('https://webdriver.io/');
+    await mainPage.url('https://webdriver.io/');
     await $('//*[@class="navbar__brand"]').click();
     await $('.DocSearch-Button-Container').click();
     await $('//*[@id="docsearch-input"]').setValue('Boilerplate Projects');
@@ -13,14 +13,14 @@ describe('Checking the web site webdriver.io', () => {
   });
 
   it('Should be a dark version of the site', async () => {
-    await browser.url('https://webdriver.io/');
+    await mainPage.url('https://webdriver.io/');
     await $('//*[@class="toggle_vylO colorModeToggle_DEke"]').click();
     await browser.setTimeout({ 'script': 2000 });
     await $('//*[@class="toggle_vylO colorModeToggle_DEke"]').click();
   });
 
   it('Should be follow the link to GitHub', async () => {
-    await browser.url('https://webdriver.io/');
+    await mainPage.url('https://webdriver.io/');
     await $('//*[@class="navbar__brand"]').click();
     await $('.navbar__item.navbar__link.header-github-link').click();
     console.log(await browser.getUrl());
